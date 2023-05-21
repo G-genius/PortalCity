bf570126d9070230677f61e548fcee7a739b977e
G-genius <gamelob25@gmail.com>
2023-05-21 14:39:31 +0300

    доделывание сервера

--- Server/index.js
+++ Server/index.js
@@ -3,11 +3,16 @@ const express = require("express")
 const sequelize = require('./db')
 const models = require('./models/models')
 const cors = require('cors')
+const fileUpload = require('express-fileupload')
+const path = require('path')
+
 const router = require('./routes/index')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(fileUpload({}))
 app.use('/api', router)
 
 app.get('/', (req, res) => {
